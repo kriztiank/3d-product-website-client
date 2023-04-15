3081143cecc1c161adf891ef4e588ebcc910cf52
kriztiank <kriztiank@gmail.com>
2023-04-15 13:09:42 +0200

    init

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://fair-tan-squirrel-cap.cyclic.app/api/v1/dalle",
+  },
+};
+
+export default config;
